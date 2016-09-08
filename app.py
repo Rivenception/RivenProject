d6cab2476ac91487877520f6160fb511bebe8c02
Rick Mingione <rbsm88@rickminsmacbook.home>
2016-09-07 21:20:16 -0400

    Prototype initial idea

---
+++ app.py
@@ -0,0 +1,93 @@
+# total_ad=ad_from_items + ad_from_runes + ad_per_level + base_ad
+# bonus_ad=ad_from_items + ad_from_runes
+# ad_from_items=raw_input
+# ad_from_runes=raw_input
+# base_ad=54.06
+# level=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)
+# ad_per_level=2.83 * level
+# skill=(1,2,3,4,5)
+# E=0
+
+total_ad = 10
+bonus_ad = 5
+
+q_level = 1
+w_level = 1
+e_level = 1
+
+def is_ult_active():
+  return True
+
+def move_q():
+  if q_level == 1:
+    if is_ult_active():
+      return 30 + 0.45 * total_ad * 0.2 * bonus_ad
+    else:
+      return 10 + 0.4 * total_ad
+  if q_level == 2:
+    if is_ult_active():
+      return 30 + 0.45 * total_ad * 0.2 * bonus_ad
+    else:
+      return 30 + 0.45 * total_ad
+  if q_level == 3:
+    if is_ult_active():
+      return 50 + .5 * total_ad * 0.2 * bonus_ad
+    else:
+      return 50 + .5 * total_ad
+  if q_level == 4:
+    if is_ult_active():
+      return 70 + 5.5 * total_ad * 0.2 * bonus_ad
+    else:
+      return 70 + 5.5 * total_ad
+  if q_level == 5:
+    if is_ult_active():
+      return 90 + 0.6 * total_ad * 0.2 * bonus_ad
+    else:
+      return 90 + 0.6 * total_ad
+
+def move_w():
+  if w_level == 1:
+    if is_ult_active():
+      bonus_ad = 1.2 * bonus_ad
+      return 50 + 1.0 * bonus_ad
+    else:
+      return 50 + 1.0 * bonus_ad
+  if w_level == 2:
+    if is_ult_active():
+      bonus_ad = 1.2 * bonus_ad * 
+      return 80 + 1.0 * bonus_ad * 
+    else:
+      return 80 + 1.0 * bonus_ad
+  if w_level == 3:
+    if is_ult_active():
+      bonus_ad = 1.2 * bonus_ad
+      return 80 + 1.0 * bonus_ad
+    else:
+      return 110 + 1.0 * bonus_ad
+  if w_level == 4:
+    if is_ult_active():
+      bonus_ad = 1.2 * bonus_ad
+      return 140 + 1.0 * bonus_ad
+    else:
+      return 140 + 1.0 * bonus_ad
+  if w_level == 5:
+    if is_ult_active():
+      bonus_ad = 1.2 * bonus_ad
+      return 170 + 1.0 * bonus_ad
+    else:
+      return 170 + 1.0 * bonus_ad
+
+def move_r():
+  if r_level == 1:
+    return 100 + 0.6 * bonus_ad
+  if r_level == 2:
+    return 150 + 0.6 * bonus_ad
+  if r_level == 3:
+    return 200 + 0.6 * bonus_ad
+
+# QW=Q + R
+# ERQ=E + R + Q
+# ER2WRQ3=E + R + 2 + W + R + Q3
+# RQ3=R + Q3
+# EWQ=E + W + Q
+
